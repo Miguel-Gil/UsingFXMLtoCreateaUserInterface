30d53547a8bd3e6603b271ddd18a0a4adfb82ba3
Miguel-Gil <hayudar@hotmail.com>
2023-10-18 13:49:32 +0200

    primer commit

---
+++ src/main/java/es/ieslosmontecillos/usingfxmltocreateauserinterface/FXMLExample.java
@@ -0,0 +1,23 @@
+package es.ieslosmontecillos.usingfxmltocreateauserinterface;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXMLExample extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(new Scene(root, 400, 375));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(FXMLExample.class, args);
+    }
+}
